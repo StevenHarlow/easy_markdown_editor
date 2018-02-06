ec9df34e1049a8929b7a35c2a3c6e5f827e3649a
Steven Harlow <stharlow@gmail.com>
2018-02-06 21:26:09 +0700

    helper inclusion

---
+++ app/helpers/editor_helper.rb
@@ -0,0 +1,10 @@
+module EditorHelper
+  def markdown_editor(content = 'hello world')
+    content_tag(:div, class: 'easyMarkdown--option') do
+      content
+    end
+    content_tag(:div, class: 'easyMarkdown--previewBtn') do
+      content
+    end
+  end
+end
--- lib/easy_markdown_editor/engine.rb
+++ lib/easy_markdown_editor/engine.rb
@@ -1,6 +1,9 @@
 module EasyMarkdownEditor
   module Rails
     class Engine < ::Rails::Engine
+      config.to_prepare do
+        ApplicationController.helper(EditorHelper)
+      end
     end
   end
 end
