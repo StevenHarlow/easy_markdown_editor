a98b4ead0ebdcd5d24ab4368d3dc65e3c670f695
Steven Harlow <stharlow@gmail.com>
2018-02-06 23:13:15 +0700

    exclusion

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -1,11 +1,39 @@
 module EditorHelper
-  def markdown_editor(content = 'hello world')
+  def markdown_editor(options)
     content_tag(:div, class: 'easyMarkdown') do
-      content_tag(:div, class: 'easyMarkdown--option') do
-        content
-      end +
-      content_tag(:div, class: 'easyMarkdown--previewBtn') do
-        content
+      yield
+      content_tag(:div, class: 'easyMarkdown--buttons') do
+        Editor.build_buttons(options)
+      end
+    end
+  end
+
+  class Editor
+    ALL_BUTTONS = [
+      :h1,
+      :h2,
+      :h3,
+      :h4,
+      :h5,
+      :italic,
+      :bold,
+      :bullet,
+      :indent,
+      :underline,
+      :table,
+      :break,
+      :block_quote,
+      :link,
+      :image
+    ]
+
+    def self.build_buttons(options)
+      buttons = ALL_BUTTONS - options[:exclude]
+      content_tag(:div, class: 'easyMarkdown--buttons') do
+        buttons.each do |b|
+          button_tag(:div, class: "easyMarkdown--#{b}") +
+        end
+        button_tag(:div, class: 'easyMarkdown--previewBtn')
       end
     end
   end
