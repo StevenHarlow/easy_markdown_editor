edbe3905be4b58038e524a2fdc404e3f63974da5
Steven Harlow <stharlow@gmail.com>
2018-02-06 23:38:30 +0700

    remove class

--- Rakefile
+++ Rakefile
@@ -4,3 +4,7 @@ require "rspec/core/rake_task"
 RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
+
+task :console do
+  exec "irb -r easy_markdown_editor -I ./lib"
+end
--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -1,40 +1,38 @@
 module EditorHelper
+  ALL_BUTTONS = [
+    :h1,
+    :h2,
+    :h3,
+    :h4,
+    :h5,
+    :italic,
+    :bold,
+    :bullet,
+    :indent,
+    :underline,
+    :table,
+    :break,
+    :block_quote,
+    :link,
+    :image
+  ]
+
   def markdown_editor(options)
     content_tag(:div, class: 'easyMarkdown') do
       yield
       content_tag(:div, class: 'easyMarkdown--buttons') do
-        Editor.build_buttons(options)
+        build_buttons(options)
       end
     end
   end
 
-  class Editor
-    ALL_BUTTONS = [
-      :h1,
-      :h2,
-      :h3,
-      :h4,
-      :h5,
-      :italic,
-      :bold,
-      :bullet,
-      :indent,
-      :underline,
-      :table,
-      :break,
-      :block_quote,
-      :link,
-      :image
-    ]
-
-    def self.build_buttons(options)
-      buttons = ALL_BUTTONS - options[:exclude]
-      content_tag(:div, class: 'easyMarkdown--buttons') do
-        buttons.reduce('') do |group,btn|
-          group << button_tag(:div, class: "easyMarkdown--btn easyMarkdown--#{b}")
-        end.html_safe +
-        button_tag(:div, class: 'easyMarkdown--previewBtn')
-      end
+  def self.build_buttons(options)
+    buttons = ALL_BUTTONS - options[:exclude]
+    content_tag(:div, class: 'easyMarkdown--buttons') do
+      buttons.reduce('') do |group,btn|
+        group << button_tag(:div, class: "easyMarkdown--btn easyMarkdown--#{b}")
+      end.html_safe +
+      button_tag(:div, class: 'easyMarkdown--previewBtn')
     end
   end
 end
